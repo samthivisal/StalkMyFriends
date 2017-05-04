b00982f9c7dc58f89f9dbe1532ea1dbdf1c6b13a
Minuscience <Minuscience@outlook.com>
2017-05-04 10:24:23 +0200

    base

--- src/main/java/com/dant/entity/Account.java
+++ src/main/java/com/dant/entity/Account.java
@@ -1,57 +1,135 @@
 package com.dant.entity;
 
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.Reference;
+
 import java.io.Serializable;
 
-/**
- * Created by pitton on 2017-02-21.
- */
+
+@Entity
 public class Account implements Serializable {
+    @Id
+    private ObjectId id;
 
-	private String email;
-	private long updated;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+    private String password;
 
-	public Account() {
-		updated = System.currentTimeMillis();
-	}
+    @Reference
+    private Position location;
+    private char[] token;
 
-	public Account(String email) {
-		this();
-		this.email = email;
-	}
+    private long updated;
 
-	public String getEmail() {
-		return email;
-	}
+    public Account(ObjectId id, String firstName, String lastName, String phoneNumber, String email, String password, Position location, char[] token, long updated) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.password = password;
+        this.location = location;
+        this.token = token;
+        this.updated = updated;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public Account() {
+        updated = System.currentTimeMillis();
+    }
 
-	public long getUpdated() {
-		return updated;
-	}
+    public String getEmail() {
+                return email;
+    }
 
-	public void setUpdated(long updated) {
-		this.updated = updated;
-	}
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
+    public ObjectId getId() {
+        return id;
+    }
 
-		Account account = (Account) o;
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
 
-		return email.equals(account.email);
-	}
+    public String getFirstName() {
+        return firstName;
+    }
 
-	@Override
-	public int hashCode() {
-		return email.hashCode();
-	}
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
 
-	@Override
-	public String toString() {
-		return email;
-	}
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Position getLocation() {
+        return location;
+    }
+
+    public void setLocation(Position location) {
+        this.location = location;
+    }
+
+    public char[] getToken() {
+        return token;
+    }
+
+    public void setToken(char[] token) {
+        this.token = token;
+    }
+
+    public long getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(long updated) {
+        this.updated = updated;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Account account = (Account) o;
+
+        return email.equals(account.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return email.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return email;
+    }
 }
---
+++ src/main/java/com/dant/entity/Position.java
@@ -0,0 +1,32 @@
+package com.dant.entity;
+
+/**
+ * Created by OPERMAN Timoty on 04/05/2017.
+ */
+public class Position {
+    private double latitude;
+    private double longitude;
+
+    public Position(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+
+}
